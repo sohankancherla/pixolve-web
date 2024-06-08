05285ca638e2f20cd25fbd5c7ddac95e45172115
skancher <sohank@gmail.com>
2024-06-07 17:44:41 -0700

    Final tailwind setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+
+import { cn } from "@/lib/utils";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,8 +27,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.variable} ${poppins.variable} font-sans`}>
+    <html lang="en">
+      <body
+        className={cn(
+          inter.variable,
+          poppins.variable,
+          "min-h-screen bg-background font-sans antialiased"
+        )}
+      >
         {children}
       </body>
     </html>
