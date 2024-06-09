95810c294d59d440a9affc6b38aac3a85aa2b5ab
skancher <sohank@gmail.com>
2024-06-08 20:50:26 -0700

    Add google sign up button to ui

--- components/signup.tsx
+++ components/signup.tsx
@@ -19,21 +19,54 @@ export default function SignUp() {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
-        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardTitle className="text-xl">Create your account</CardTitle>
         <CardDescription>
-          Enter your information to create an account
+          Please fill in the details to get started
         </CardDescription>
       </CardHeader>
       <CardContent>
         <div className="grid gap-4">
+          <Button variant="outline" className="w-full">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 48 48"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              className="h-4 w-4 mr-2"
+            >
+              <path
+                fill="#EA4335"
+                d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+              />
+              <path
+                fill="#4285F4"
+                d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+              />
+              <path
+                fill="#FBBC05"
+                d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+              />
+              <path
+                fill="#34A853"
+                d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+              />
+              <path fill="none" d="M0 0h48v48H0z" />
+            </svg>
+            Sign up with Google
+          </Button>
+          <div className="flex mt-2 mb-2 w-full items-center justify-center">
+            <div className="bg-input w-full h-[1px]" />
+            <span className="text-muted-foreground text-sm px-6">or</span>
+            <div className="bg-input w-full h-[1px]" />
+          </div>
           <div className="grid grid-cols-2 gap-4">
             <div className="grid gap-2">
               <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+              <Input id="first-name" placeholder="John" required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
+              <Input id="last-name" placeholder="Doe" required />
             </div>
           </div>
           <div className="grid gap-2">
@@ -41,7 +74,7 @@ export default function SignUp() {
             <Input
               id="email"
               type="email"
-              placeholder="m@example.com"
+              placeholder="jdoe@example.com"
               required
             />
           </div>
@@ -52,14 +85,11 @@ export default function SignUp() {
           <Button type="submit" className="w-full">
             Create an account
           </Button>
-          <Button variant="outline" className="w-full">
-            Sign up with GitHub
-          </Button>
         </div>
         <div className="mt-4 text-center text-sm">
           Already have an account?{' '}
           <Link href="/app" className="underline">
-            Sign in
+            Log in
           </Link>
         </div>
       </CardContent>
