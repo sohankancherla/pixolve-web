8058d427626affca56d677814c94f1757c3ebd26
skancher <sohank@gmail.com>
2024-06-19 19:50:23 -0700

    remove hover effect from navigation bar

--- components/app/sidebar.tsx
+++ components/app/sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -29,12 +29,12 @@ const icons = {
 };
 
 const navItemsTop = [
-  { name: 'Home', href: '/app', icon: 'home' },
+  // { name: 'Home', href: '/app', icon: 'home' },
   { name: 'Enhance', href: '/app/enhance', icon: 'wandSparkles' },
-  { name: 'Deblur', href: '/app/deblur', icon: 'droplet' },
+  // { name: 'Deblur', href: '/app/deblur', icon: 'droplet' },
   { name: 'Merge', href: '/app/merge', icon: 'merge' },
   { name: 'Find', href: '/app/find', icon: 'squareDashedMousePointer' },
-  { name: 'Group', href: '/app/group', icon: 'images' },
+  // { name: 'Group', href: '/app/group', icon: 'images' },
 ];
 
 const navItemsBottom = [
@@ -44,12 +44,10 @@ const navItemsBottom = [
 export function SidebarItem({
   href,
   icon,
-  sidebarRef = null,
   children,
 }: {
   href: string;
   icon: string;
-  sidebarRef: React.RefObject<HTMLDivElement> | null;
   children: React.ReactNode;
 }) {
   const Icon = icons[icon as keyof typeof icons];
@@ -63,24 +61,23 @@ export function SidebarItem({
         aria-current={pathname === href ? 'page' : undefined}
         aria-label={`Navigate to ${children}`}
         className={cn(
-          'flex items-center w-full py-4 px-[9.5px] rounded-full lg:hover:bg-accent/50 group/link',
+          'flex flex-col sm:flex-row items-center w-[83px]  sm:w-full p-2 sm:py-4 sm:px-3 rounded-full lg:hover:bg-accent/50 group/link',
           pathname === href && 'sm:bg-accent',
         )}
-        onClick={() => sidebarRef?.current?.classList.remove('lg:hover:w-52')}
       >
         <Icon
-          strokeWidth={1.75}
           className={cn(
             'h-6 w-6 sm:h-5 sm:w-5 mx-1 flex-shrink-0 stroke-muted-foreground lg:group-hover/link:stroke-foreground',
-            pathname === href && 'stroke-violet-500',
+            pathname === href &&
+              'stroke-violet-500 lg:group-hover/link:stroke-violet-500',
           )}
           aria-hidden="true"
         />
         <span
           className={cn(
-            'hidden lg:block font-display text-muted-foreground leading-5 tracking-tight ml-6 group-hover/link:text-foreground group-hover/link:translate-x-2 transistion-all duration-300 ease-in-out motion-reduce:transition-none motion-reduce:translate-x-0',
+            'font-display text-muted-foreground leading-5 tracking-tight mt-2 sm:mt-0 sm:ml-6 lg:group-hover/link:text-foreground lg:group-hover/link:translate-x-2 lg:transistion-all duration-300 ease-in-out motion-reduce:transition-none motion-reduce:translate-x-0',
             pathname === href &&
-              'text-transparent bg-clip-text bg-gradient-to-br from-violet-500 to-pink-400',
+              'text-violet-500 sm:text-transparent sm:bg-clip-text sm:bg-gradient-to-br from-violet-500 to-pink-400 lg:group-hover/link:text-transparent',
           )}
         >
           {children}
@@ -91,14 +88,10 @@ export function SidebarItem({
 }
 
 export default function Sidebar() {
-  const sidebarRef = useRef<HTMLDivElement>(null);
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
     <nav
-      ref={sidebarRef}
-      className="flex small:block fixed bottom-0 right-0 left-0 sm:static sm:min-h-screen sm:w-16 lg:hover:w-52 lg:overflow-x-hidden px-2 py-2 sm:py-6 border-t sm:border-r border-gray-200 dark:border-gray-800 shadow-lg dark:shadow-black transition-all duration-500 ease-in-out motion-reduce:transition-none group/nav"
+      className="flex small:block fixed bottom-0 right-0 left-0 sm:static sm:min-h-screen sm:w-56 px-2 py-2 sm:py-6 border-t sm:border-r border-gray-200 dark:border-gray-800 shadow-lg dark:shadow-black group/nav"
       aria-label="Main Navigation"
-      onMouseLeave={() => sidebarRef.current?.classList.add('lg:hover:w-52')}
     >
       <section className="flex flex-col h-full w-full">
         <Link
@@ -107,34 +100,24 @@ export default function Sidebar() {
           aria-label="Pixolve Home"
         >
           <Image
-            src="/icon-purple.png"
+            src="/logo-purple.png"
             alt="Pixolve"
-            width="28"
-            height="28"
-            className="mx-[9.5px]"
+            width="120"
+            height="22"
+            className="mx-4"
           />
         </Link>
-        <div className="flex sm:flex-col h-full w-full sm:justify-between gap-2 justify-center">
-          <ul className="cursor-pointer flex justify-around sm:flex-col gap-2">
+        <div className="flex sm:flex-col h-full w-full sm:justify-between sm:py-2">
+          <ul className="cursor-pointer flex justify-around sm:flex-col sm:gap-2 flex-[3] sm:flex-none">
             {navItemsTop.map((item) => (
-              <SidebarItem
-                key={item.name}
-                href={item.href}
-                icon={item.icon}
-                sidebarRef={sidebarRef}
-              >
+              <SidebarItem key={item.name} href={item.href} icon={item.icon}>
                 {item.name}
               </SidebarItem>
             ))}
           </ul>
-          <ul className="cursor-pointer flex justify-around sm:flex-col gap-2">
+          <ul className="cursor-pointer flex justify-around sm:flex-col sm:gap-2 flex-1 sm:flex-none">
             {navItemsBottom.map((item) => (
-              <SidebarItem
-                key={item.name}
-                href={item.href}
-                icon={item.icon}
-                sidebarRef={sidebarRef}
-              >
+              <SidebarItem key={item.name} href={item.href} icon={item.icon}>
                 {item.name}
               </SidebarItem>
             ))}
