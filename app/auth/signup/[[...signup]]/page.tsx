b1200a3e469514000a36d6d98200401e82a12994
skancher <sohank@gmail.com>
2024-07-05 11:46:14 -0700

    Add clerk sign up styles back

--- app/auth/signup/[[...signup]]/page.tsx
+++ app/auth/signup/[[...signup]]/page.tsx
@@ -2,12 +2,54 @@
 
 import React from 'react';
 
+import { useTheme } from 'next-themes';
+
 import { SignUp } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+
+import { lightTheme, darkTheme } from '@/lib/colors';
 
 export default function SignUpPage() {
+  const { resolvedTheme } = useTheme();
+
   return (
     <div className="min-h-[552px]">
-      <SignUp />
+      <SignUp
+        appearance={{
+          baseTheme: resolvedTheme === 'dark' ? dark : undefined,
+          layout: {
+            logoPlacement: 'none',
+          },
+          variables: {
+            colorPrimary:
+              resolvedTheme === 'dark' ? darkTheme.primary : lightTheme.primary,
+            colorDanger:
+              resolvedTheme === 'dark'
+                ? darkTheme.destructive
+                : lightTheme.destructive,
+            colorText:
+              resolvedTheme === 'dark'
+                ? darkTheme.cardForeground
+                : lightTheme.cardForeground,
+            colorTextOnPrimaryBackground:
+              resolvedTheme === 'dark'
+                ? darkTheme.primaryForeground
+                : lightTheme.primaryForeground,
+            colorTextSecondary:
+              resolvedTheme === 'dark'
+                ? darkTheme.mutedForeground
+                : lightTheme.mutedForeground,
+            colorBackground:
+              resolvedTheme === 'dark' ? darkTheme.card : lightTheme.card,
+            fontSize: '14px',
+            borderRadius: lightTheme.radius,
+          },
+          elements: {
+            cardBox: `sm:shadow-none border border-${resolvedTheme === 'dark' ? 'dark' : 'light'}`,
+            header: 'text-left text-xl tracking-tight',
+          },
+        }}
+      />
     </div>
   );
 }
