e139458a1eceaf6fc36853e61fae7d25a6bcd7b8
skancher <sohank@gmail.com>
2024-07-15 11:40:58 -0700

    test github repo migration

--- app/dashboard/enhance/page.tsx
+++ app/dashboard/enhance/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import UserButton from '@/components/dashboard/user-button';
+import ImageUploader from '@/components/ui/image-uploader';
 
 export const metadata = {
   title: 'Enhance',
@@ -8,9 +9,14 @@ export const metadata = {
 
 export default function EnhancePage() {
   return (
-    <div className="w-full flex justify-between items-center p-4">
-      <h1 className="text-4xl font-medium">Enhance</h1>
-      <UserButton />
-    </div>
+    <>
+      <div className="w-full flex justify-between items-center p-4">
+        <h1 className="text-4xl font-medium">Enhance</h1>
+        <UserButton />
+      </div>
+      <div className="p-4 mt-10">
+        <ImageUploader />
+      </div>
+    </>
   );
 }
