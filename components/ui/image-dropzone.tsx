2d55df7ae4da2dc2f07151deb8c74e64090adf32
skancher <sohank@gmail.com>
2024-08-29 09:01:17 -0700

    Make image upload responsive

--- components/ui/image-dropzone.tsx
+++ components/ui/image-dropzone.tsx
@@ -5,23 +5,14 @@ import { useDropzone } from 'react-dropzone';
 import Image from 'next/image';
 import { ArrowUpTrayIcon, MinusIcon } from '@heroicons/react/24/outline';
 import { Button } from './button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
 
 export default function ImageDropzone() {
   const [files, setFiles] = useState<File[]>([]);
-  const [folderUploaded, setFolderUploaded] = useState(false);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length > 0) {
-      setFiles(acceptedFiles);
-      setFolderUploaded(true);
+      setFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
     }
   }, []);
 
@@ -42,31 +33,37 @@ export default function ImageDropzone() {
   };
 
   return (
-    <Card className="flex flex-col h-full border-none shadow-none">
-      <CardContent className="h-full min-h-80">
+    <div className="flex flex-col flex-grow overflow-hidden">
+      <div className="flex-grow overflow-hidden">
         {files.length > 0 ? (
-          <div className="grid grid-cols-4 gap-4">
-            {files.map((file) => (
-              <div
-                key={file.name}
-                className="flex flex-col gap-2 items-start h-52"
-              >
-                <div className="relative w-full h-full">
-                  <Image
-                    src={URL.createObjectURL(file)}
-                    alt={file.name}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-md"
-                  />
-                  <button
-                    onClick={() => removeFile(file.name)}
-                    className="absolute top-1 right-1 p-1 rounded-full bg-gray-600 text-white hover:bg-gray-800"
-                  >
-                    <MinusIcon className="h-4 w-4" />
-                  </button>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 rounded-md border-2 border-dashed h-full p-4 overflow-y-auto content-start">
+            <div
+              {...getRootProps()}
+              className="flex flex-col items-center justify-center rounded-md border-2 border-dashed cursor-pointer"
+            >
+              <input {...getInputProps()} />
+              <div className="flex flex-col items-center justify-center">
+                <div className="p-4 border border-dashed rounded-full mb-4">
+                  <ArrowUpTrayIcon className="h-6 w-6 text-muted-foreground" />
                 </div>
-                <p className="text-sm text-muted-foreground">{file.name}</p>
+                <span className="text-muted-foreground/50 text-sm text-center">
+                  Upload up to 100 images
+                </span>
+              </div>
+            </div>
+            {files.map((file) => (
+              <div key={file.name} className="relative w-full h-52">
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt={file.name}
+                  className="rounded-md object-cover h-full w-full"
+                />
+                <button
+                  onClick={() => removeFile(file.name)}
+                  className="absolute top-1 right-1 p-1 rounded-md bg-gray-600 text-white hover:bg-gray-800"
+                >
+                  <MinusIcon className="h-4 w-4" />
+                </button>
               </div>
             ))}
           </div>
@@ -81,21 +78,21 @@ export default function ImageDropzone() {
                 <ArrowUpTrayIcon className="h-6 w-6 text-muted-foreground" />
               </div>
               <span className="sr-only">Upload</span>
-              <span className="text-muted-foreground">
+              <span className="text-muted-foreground text-center mb-1 px-2">
                 {isDragActive
                   ? 'Drop the files here ...'
                   : "Drag 'n' drop a folder here, or click to select images"}
               </span>
-              <span className="text-muted-foreground/50 text-sm">
+              <span className="text-muted-foreground/50 text-sm text-center">
                 Upload up to 100 images
               </span>
             </div>
           </div>
         )}
-      </CardContent>
-      <CardFooter className="flex justify-end">
+      </div>
+      <div className="flex justify-end py-4">
         <Button>Generate</Button>
-      </CardFooter>
-    </Card>
+      </div>
+    </div>
   );
 }
