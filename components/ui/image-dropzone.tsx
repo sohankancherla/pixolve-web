810e5ec69e84cdbaa864d42c8cb4be320d7e43d4
skancher <sohank@gmail.com>
2024-08-31 18:21:12 -0700

    Add loading and cluster api

--- components/ui/image-dropzone.tsx
+++ components/ui/image-dropzone.tsx
@@ -5,6 +5,10 @@ import { useDropzone } from 'react-dropzone';
 import Image from 'next/image';
 import { ArrowUpTrayIcon, MinusIcon } from '@heroicons/react/24/outline';
 import { Button } from './button';
+import JSZip from 'jszip';
+import axios from 'axios';
+import { useAuth } from '@clerk/nextjs';
+import { Loader2Icon } from 'lucide-react';
 
 interface FileItem {
   file: File;
@@ -12,8 +16,10 @@ interface FileItem {
 }
 
 export default function ImageDropzone() {
+  const [isLoading, setIsLoading] = useState(false);
   const [files, setFiles] = useState<Map<number, FileItem>>(new Map());
   const nextIdRef = useRef(0);
+  const { getToken } = useAuth();
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length > 0) {
@@ -59,6 +65,35 @@ export default function ImageDropzone() {
     });
   }, []);
 
+  const handleClick = async () => {
+    setIsLoading(true);
+    const zip = new JSZip();
+
+    Array.from(files.values()).forEach((fileItem) => {
+      zip.file(fileItem.file.name, fileItem.file);
+    });
+
+    const zipBlob = await zip.generateAsync({ type: 'blob' });
+
+    const formData = new FormData();
+    formData.append('file', zipBlob, 'images.zip');
+
+    axios
+      .post('http://localhost:8000/cluster', formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
+
   const fileItems = useMemo(() => {
     return Array.from(files.entries()).map(([id, { file, url }]) => (
       <div key={id} className="relative w-full h-52">
@@ -95,7 +130,11 @@ export default function ImageDropzone() {
           <ArrowUpTrayIcon className="h-6 w-6 text-muted-foreground" />
         </div>
         <span className="text-muted-foreground text-sm text-center">
-          {isLimitReached ? 'Upload limit reached' : 'Upload files'}
+          {isDragActive
+            ? 'Drop the files here ...'
+            : isLimitReached
+              ? 'Upload limit reached'
+              : 'Upload files'}
         </span>
         <span className="text-muted-foreground/50 text-sm text-center">
           {files.size}/100
@@ -105,39 +144,48 @@ export default function ImageDropzone() {
   );
 
   return (
-    <div className="flex flex-col flex-grow overflow-hidden">
-      <div className="flex-grow overflow-hidden">
-        {files.size > 0 ? (
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 rounded-md border-2 border-dashed h-full p-4 overflow-y-auto content-start">
-            {renderUploadArea()}
-            {fileItems}
-          </div>
-        ) : (
-          <div
-            {...getRootProps()}
-            className="flex flex-col aspect-square h-full w-full items-center justify-center rounded-md border-2 border-dashed cursor-pointer mb-4"
-          >
-            <input {...getInputProps()} />
-            <div className="flex flex-col items-center justify-center">
-              <div className="p-4 border border-dashed rounded-full mb-4">
-                <ArrowUpTrayIcon className="h-6 w-6 text-muted-foreground" />
-              </div>
-              <span className="sr-only">Upload</span>
-              <span className="text-muted-foreground text-center mb-1 px-2">
-                {isDragActive
-                  ? 'Drop the files here ...'
-                  : "Drag 'n' drop a folder here, or click to select images"}
-              </span>
-              <span className="text-muted-foreground/50 text-sm text-center">
-                Upload up to 100 images
-              </span>
+    <>
+      {isLoading && (
+        <div className="fixed h-screen w-screen bg-gray-500 bg-opacity-50 backdrop-blur z-10 left-0 top-0 flex items-center justify-center">
+          <Loader2Icon className="h-10 w-10 animate-spin text-primary" />
+        </div>
+      )}
+      <div className="flex flex-col flex-grow overflow-hidden">
+        <div className="flex-grow overflow-hidden">
+          {files.size > 0 ? (
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 rounded-md border-2 border-dashed h-full p-4 overflow-y-auto content-start">
+              {renderUploadArea()}
+              {fileItems}
             </div>
-          </div>
-        )}
+          ) : (
+            <div
+              {...getRootProps()}
+              className="flex flex-col aspect-square h-full w-full items-center justify-center rounded-md border-2 border-dashed cursor-pointer mb-4"
+            >
+              <input {...getInputProps()} />
+              <div className="flex flex-col items-center justify-center">
+                <div className="p-4 border border-dashed rounded-full mb-4">
+                  <ArrowUpTrayIcon className="h-6 w-6 text-muted-foreground" />
+                </div>
+                <span className="sr-only">Upload</span>
+                <span className="text-muted-foreground text-center mb-1 px-2">
+                  {isDragActive
+                    ? 'Drop the files here ...'
+                    : "Drag 'n' drop a folder here, or click to select images"}
+                </span>
+                <span className="text-muted-foreground/50 text-sm text-center">
+                  Upload up to 100 images
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="flex justify-end py-4">
+          <Button onClick={handleClick} disabled={files.size < 2}>
+            Generate
+          </Button>
+        </div>
       </div>
-      <div className="flex justify-end py-4">
-        <Button>Generate</Button>
-      </div>
-    </div>
+    </>
   );
 }
